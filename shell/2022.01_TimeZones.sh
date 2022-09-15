3fd7db1752138572853f4c61bd933d143fedc812
Aaron McGinn <aaron.mcginn@cerner.com>
2022-09-15 17:10:00 -0500

    Remove unnecessary comments from 2022.01_TimeZones

--- shell/2022.01_TimeZones.sh
+++ shell/2022.01_TimeZones.sh
@@ -9,7 +9,6 @@ echo "Here are the current times for the major Oracle Cerner office locations:"
 for zone in "${zones[@]}"; do
   id=`echo $zone |  cut -d'|' -f 1`
   region=`echo $zone |  cut -d'|' -f 2`
-  #echo "${id} is in ${region}"
 
   zone_datetime=`TZ="$id" date "+%a %d %b %H:%M"`
   echo "It is ${zone_datetime} at the Oracle Cerner offices in ${region}!"
